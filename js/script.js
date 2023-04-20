b04781f08a7645e2d6ab09b2b95be91b6010daff
Cynox336 <cynox336@gmail.com>
2023-04-20 11:02:43 +0200

    Update 20/04/23 11:02

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 //En este array de strings, tenemos todos los colores
 const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
-
-
+var clickButton = document.querySelector(".btn-outline-secondary");
+clickButton.addEventListener("click", function() {
+  var randomColorIndex = Math.floor(Math.random() * colors.length);
+  var randomColor = colors[randomColorIndex];
+  document.body.style.backgroundColor = randomColor;
+  console.log("Me has hecho click");
+});
